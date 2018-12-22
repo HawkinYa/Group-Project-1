2452045c0c87e373aca158ccee3126a7a606e132
satinder <satinder042890@gmail.com>
2018-12-22 11:16:56 -0700

    css file changed

--- assets/app.js
+++ assets/app.js
@@ -17,7 +17,7 @@ function loadvideos(queryString){
     });
 }
 function loadBooks(){
-    var h="html basics";
+    var h="basic html for beginners";
     $.getJSON("https://www.googleapis.com/books/v1/volumes?q="+h, function(data){
         console.log(data);
         showBooks(data);
@@ -47,14 +47,17 @@ function reactvideos(){
     
 function showVideos(data){
     $("main").empty();
+    console.log(data);
     for(var i=0; i<data.items.length; i++){
         $("main").append("<article><iframe width='200px' height='100px' src='https://www.youtube.com/embed/"+data.items[i].id.videoId+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe><div class='details'><h4>"+data.items[i].snippet.title+"</h4><p>"+data.items[i].snippet.description.substring(0,100)+"</p></div></article>");
      }
 }
 
 function showBooks(data){
+    console.log(data);
     $("main").empty();
-    for(var i=0; i<data.items.length; i++){
+    for(var i=0; i<10; i++){
+        console.log("no "+i+"."+data.items[i].volumeInfo.description);
         $("main").append("<article><img src='"+data.items[i].volumeInfo.imageLinks.thumbnail+"'><div class='details'><h4>"+data.items[i].volumeInfo.title+"</h4><p>"+data.items[i].volumeInfo.description.substring(0,100)+"</p><a href='"+data.items[i].volumeInfo.infoLink+"'><button>Read More</button></a></div></article>");
      }
 }
