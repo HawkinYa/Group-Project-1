5c715716a8234b63d0af8d4de21d3c39ec5323b8
Xybolx <xybolx@gmail.com>
2019-01-07 07:59:44 -0700

    users connected

--- assets/app.js
+++ assets/app.js
@@ -62,7 +62,7 @@ function resetForm() {
 
 //Function to store UserName and Feedback values in the database
 function storeValues() {
-    database.ref().push({
+    database.ref("/store").push({
         name: name,
         feedback: feedback,
         turnStyle: turnStyle
@@ -151,7 +151,7 @@ $('#Feedback').on('input', function () {
 
 
 //Function to listen for additions to database and append them to Reviews section
-database.ref().on("child_added", function (snapChild) {
+database.ref("/store").on("child_added", function (snapChild) {
     console.log(snapChild.val().feedback);
     console.log(snapChild.val().name);
     console.log(snapChild.val().turnStyle);
@@ -159,7 +159,47 @@ database.ref().on("child_added", function (snapChild) {
     var row = $("<tr>");
     row.append('<div class="card"><div class="card-header" id="reviewHaeder">' + snapChild.val().name + '</div><div class="card-body"><p class="card-text">' + snapChild.val().feedback + '</p></div></div><br>');
     $("tbody").append(row);
-    $("#visitors").text("Visitors" + turnCount);
+    // $("#visitors").text("Visitors" + turnCount);
+});
+
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+    
+    // If they are connected..
+    if (snap.val()) {
+        
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+        
+        
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+    }
+});
+
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snapshot) {
+    
+    // Display the viewer count in the html.
+    // The number of online users is the number of children in the connections list.
+    var watchers = snapshot.numChildren();
+    $("#visitors").text( " " + watchers + " " + "Connected" + "User(s)" );
+    
+
+   
+    
+    
+
+    
+
 });
 
 
